b023686a005642cb060df97d50ac5060ccdf9a5a
Hiiragi Russell Tsubasa <97942736+Hiiragi283@users.noreply.github.com>
2022-12-03 13:41:07 +0900

    Add new integration for Astral Sorcery

--- src/main/java/hiiragi283/gohd_tweaks/recipes/IntegrationCore.java
+++ src/main/java/hiiragi283/gohd_tweaks/recipes/IntegrationCore.java
@@ -5,8 +5,26 @@ import net.minecraftforge.fml.common.Loader;
 
 public class IntegrationCore {
     public static void Init() {
-        //HaCとの連携レシピの登録
-        ClimateRecipe.Init();
+        //Astral Sorceryが読み込まれている場合
+        if (Loader.isModLoaded("astralsorcery")) {
+            try {
+                //ASとの連携レシピの登録
+                AstralRecipe.Init();
+            } catch (Exception e) {
+                //失敗した場合、エラーをログに吐く
+                GOHDTweaks.LoggerGOHD.info("Loading AstralRecipe is failed!");
+            }
+        }
+        //Heat And Climateが読み込まれている場合
+        if (Loader.isModLoaded("dcs_climate")) {
+            try {
+                //HaCとの連携レシピの登録
+                ClimateRecipe.Init();
+            } catch (Exception e) {
+                //失敗した場合、エラーをログに吐く
+                GOHDTweaks.LoggerGOHD.info("Loading ClimateRecipe is failed!");
+            }
+        }
         //Thermal Expansionが読み込まれている場合
         if (Loader.isModLoaded("thermal_expansion")) {
             try {
--- src/main/java/hiiragi283/gohd_tweaks/util/GOHDUtils.java
+++ src/main/java/hiiragi283/gohd_tweaks/util/GOHDUtils.java
@@ -2,9 +2,17 @@ package hiiragi283.gohd_tweaks.util;
 
 import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 
 public class GOHDUtils {
+
+    //液体名からFluidを取得するメソッド
+    public static Fluid getFluid(String name) {
+        return net.minecraftforge.fluids.FluidRegistry.getFluid(name);
+    }
+
+    //ResourceLocationからItemを取得するメソッド
     public static Item getItem(String domain, String path) {
         return ForgeRegistries.ITEMS.getValue(new ResourceLocation(domain, path));
     }
